9f9c099ffe3a3b9bed33d1f574d816ba7e3a5c78
Robert Barta <92293708+robert-barta@users.noreply.github.com>
2022-05-09 09:19:35 +0200

    ok

--- [SOURCE-CODE]/[JAVA]/CLIENT/src/viewmodel/ViewModelFactory.java
+++ [SOURCE-CODE]/[JAVA]/CLIENT/src/viewmodel/ViewModelFactory.java
@@ -6,14 +6,21 @@ public class ViewModelFactory
 {
 
   private CustomerViewModel customerViewModel;
+  private StartViewModel startViewModel;
 
   public ViewModelFactory(Model model)
   {
     this.customerViewModel = new CustomerViewModel(model);
+    this.startViewModel = new StartViewModel(model);
   }
 
   public CustomerViewModel getCustomerViewModel()
   {
     return customerViewModel;
+  }
+
+  public StartViewModel getStartViewModel()
+  {
+    return startViewModel;
   }
 }
